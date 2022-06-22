22413e37ba3d9e7f815c6a6c8dd6466f6c0ea718
Samater Phi <Samaterphi77@gmail.com>
2022-06-22 01:06:09 -0700

    Enum empty removed

--- src/IntermediateTictactoe.sol
+++ src/IntermediateTictactoe.sol
@@ -16,7 +16,7 @@ uint8 public makeMoveCounter = 0;
 address immutable public playerOne;
 address immutable public playerTwo;
 
-enum AllowedPlays{Empty, X, O}
+enum AllowedPlays{X, O}
 
 //Stores number of plays per player to ensure turns
 //NBBB: FIND A WAY TO UPDATE THIS MAPPING AFTER EVERY PLAY
